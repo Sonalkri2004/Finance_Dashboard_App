d4cdcfe7c4e16f7755ab93233a2ae3c5dd6db6e0
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-09-28 18:47:03 +0530

    sidebar component added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import { Route, Routes } from "react-router-dom";
+import Sidebar from "./components/common/Sidebar";
 import OverviewPage from "./pages/Overviewpage";
 import ProductsPage from "./pages/ProductsPage";
 import UsersPage from "./pages/UsersPage"
@@ -20,7 +21,8 @@ function App() {
 				<div className='absolute inset-0 backdrop-blur-sm' />
 			</div>
 
-			
+			<Sidebar/>
+
 			<Routes>
 				<Route path='/' element={<OverviewPage />} />
 				<Route path='/products' element={<ProductsPage />} />
